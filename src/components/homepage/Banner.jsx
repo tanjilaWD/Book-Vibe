dab139212b933ff50c2f945ca99c6a7055841bd0
Tanjila Khatun <tanjilabithi12@gmail.com>
2026-04-18 23:32:44 +0600

    feat: Banner section

---
+++ src/components/homepage/Banner.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import bookImage from '../../assets/BannerImg.png';
+const Banner = () => {
+    return (
+        <div className="hero bg-base-200 min-h-[70vh] rounded-2xl my-10 container mx-auto">
+  <div className="hero-content flex-col lg:flex-row-reverse w-full justify-between">
+    <img
+      src={bookImage}
+     
+    />
+    <div>
+      <h1 className="text-5xl font-bold">Books to freshen up <br /> your bookshelf</h1>
+     
+      <button className="btn btn-success mt-6">View The List</button>
+    </div>
+  </div>
+</div>
+    );
+};
+
+export default Banner;
--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from '../../components/homepage/Banner';
 
 const Homepage = () => {
     return (
         <div>
-            HomePage
+           <Banner/>
         </div>
     );
 };
